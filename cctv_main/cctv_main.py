6ad89a3159c05a365a5a650c526eaa52d6897330
jh00817 <dreambait011@naver.com>
2023-07-12 11:41:22 +0900

    .

--- cctv_main/cctv_main.py
+++ cctv_main/cctv_main.py
@@ -5,20 +5,21 @@ import Object_detect_OpenCV.record
 
 max_degree = 180
 min_degree = 0
+degree = 0
 
 # 상시 녹화
 Object_detect_OpenCV.record.record("stream",100)
 
-while False : # 감지되면 
-    mootr_data = Object_detect_OpenCV.main.function_detect()
+while True : # 감지되면 
+    detect_data = Object_detect_OpenCV.main.function_detect()
     
-    if moter_data == "left" :
+    if detect_data == "left" :
         if max_degree < degree :
             print("끝")
         # 왼쪽으로 모터 조정
         else :
             print("왼쪽으로 조정중")
-    elif motor_data == "right" :
+    elif detect_data == "right" :
         # 오른쪽으로 모터 조정
         if min_degree > degree :
             print("끝")
